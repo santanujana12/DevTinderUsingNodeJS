55817a5258257d2bf38319794019b8b6ce42197d
santanujana12 <santanu.jana@aol.com>
2025-04-01 04:16:15 +0530

    Learnt to use middlewares

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import { authMiddleware } from './MiddleWares/authMiddleWare.js';
 
 const app = express();
+
+app.get("/auth", authMiddleware,(req,res)=>{
+  res.send("IsAuthorized")
+})
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!');
 });
+
+app.post("/user", (req, res, next) => {
+  next();
+  res.send("User1");
+}, (req, res) => {
+  res.send("Second function");
+})
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
